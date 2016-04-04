85838f7a4c210e141649116f3282b5194d5760fb
portree_kid <midnight.ploughboy@gmail.com>
2016-04-04 08:00:17 +0200

    Splitting of groundnets into seperate files per airport

--- WED14_to_FGAITM2.py
+++ WED14_to_FGAITM2.py
@@ -25,7 +25,7 @@
 '''
 Convert WED-1.4 Ramps and AI TaxiRoute to FG AI TrafficManagerII groundnet
 Run this script in directory with earth.wed.xml
-Groundnet will be written in newGroundNet.xml
+Groundnet will be written to files ICAO.groundnet.xml
 '''
 
 import xml.etree.ElementTree as ET
@@ -55,82 +55,96 @@ def main():
     filexml = ET.parse  ("earth.wed.xml")
     objects = filexml.getroot().find("objects")
     
-    #Collect
-    TaxiRouteNode = []
-    for obj in objects.findall("*[@class='WED_TaxiRouteNode']"):
-        point = obj.find ("point")
-        TaxiRouteNode.append ({'id': obj.get ("id"), \
-                              'lat': latNS (eval(point.get("latitude"))), \
-                              'lon': lonEW (eval(point.get("longitude")))})
-    
-    TaxiRoute = []
-    for obj in objects.findall("*[@class='WED_TaxiRoute']"):
-        beg, en = obj.find ("sources").findall ("source")
-        TaxiRoute.append ({'begin':beg.attrib["id"], \
-                             'end':en.attrib["id"], \
-                            'name':obj.find("hierarchy").attrib["name"], \
-                          'oneway':obj.find("taxi_route").attrib["oneway"] \
-                          })
+    #Iterate over airports
+    for airport in objects.findall("*[@class='WED_Airport']"):
+        airport_id = airport.get("id")
+        airport_name = airport.find("hierarchy").get("name")
+        airport_icao = airport.find("airport").get("icao")
         
-    RampPosition = []
-    for obj in objects.findall("*[@class='WED_RampPosition']"):
-        RampPosition.append({ \
-                 'id': obj.attrib['id'], \
-               'name': obj.find("hierarchy").get("name"), \
-                'lat': latNS (eval(obj.find("point").get("latitude"))), \
-                'lon': lonEW (eval(obj.find("point").get("longitude"))), \
-            'heading': obj.find("point").get("heading"), \
-               'type': obj.find("ramp_start").get("type") 
-                            })
-    
-    #Make XML
-    groundnet = ET.Element("groundnet")
-    
-    parkingList = ET.SubElement (groundnet, "parkingList")
-    for ramp in RampPosition:
-        Parking = ET.SubElement(parkingList, "Parking")
-        Parking.set ("index", ramp['id'] )
-        Parking.set ("name", ramp['name'])
-        Parking.set ("lat", ramp['lat'])
-        Parking.set ("lon", ramp['lon'])
-        Parking.set ("heading", ramp['heading'])
-        #dummy below
-        Parking.set ("type", "gate")
-        Parking.set ("number", "")
-        Parking.set ("radius", "40")
-        Parking.set ("airlineCodes", "")
-    
-    TaxiNodes = ET.SubElement (groundnet, "TaxiNodes")
-    for point in TaxiRouteNode:
-        node = ET.SubElement(TaxiNodes, "node")
-        node.set ("index", point['id'])
-        node.set ("lat", point['lat'])
-        node.set ("lon", point['lon'])
-        node.set ("isOnRunway", "0")
-        node.set ("holdPointType", "none")
-    
-    TaxiWaySegments = ET.SubElement (groundnet, "TaxiWaySegments")
-    for route in TaxiRoute:
-        arc = ET.SubElement (TaxiWaySegments, "arc")
-        arc.set ("begin", route['begin'])
-        arc.set ("end", route['end'])
-        arc.set ("isPushBackRoute", "0")
-        arc.set ("name", route['name'])
-    
-        #reverse arc
-        if route['oneway'] <> "1":
+        #Collect
+        TaxiRouteNode = []
+        for obj in objects.findall("*[@class='WED_TaxiRouteNode']"):
+            if obj.get("parent_id") != airport_id:
+                continue
+            point = obj.find ("point")
+            TaxiRouteNode.append ({'id': obj.get ("id"), \
+                                  'lat': latNS (eval(point.get("latitude"))), \
+                                  'lon': lonEW (eval(point.get("longitude")))})
+        
+        TaxiRoute = []
+        for obj in objects.findall("*[@class='WED_TaxiRoute']"):
+            if obj.get("parent_id") != airport_id:
+                continue
+            beg, en = obj.find ("sources").findall ("source")
+            TaxiRoute.append ({'begin':beg.attrib["id"], \
+                                 'end':en.attrib["id"], \
+                                'name':obj.find("hierarchy").attrib["name"], \
+                              'oneway':obj.find("taxi_route").attrib["oneway"] \
+                              })
+            
+        RampPosition = []
+        for obj in objects.findall("*[@class='WED_RampPosition']"):
+            if obj.get("parent_id") != airport_id:
+                continue
+            RampPosition.append({ \
+                     'id': obj.attrib['id'], \
+                   'name': obj.find("hierarchy").get("name"), \
+                    'lat': latNS (eval(obj.find("point").get("latitude"))), \
+                    'lon': lonEW (eval(obj.find("point").get("longitude"))), \
+                'heading': obj.find("point").get("heading"), \
+                   'type': obj.find("ramp_start").get("type") 
+                                })
+        
+        #Make XML
+        groundnet = ET.Element("groundnet")
+        groundnet.append(ET.Comment("Groundnet for %s (%s)" % (airport_name,airport_icao)))
+        
+        parkingList = ET.SubElement (groundnet, "parkingList")
+        for ramp in RampPosition:
+            Parking = ET.SubElement(parkingList, "Parking")
+            Parking.set ("index", ramp['id'] )
+            Parking.set ("name", ramp['name'])
+            Parking.set ("lat", ramp['lat'])
+            Parking.set ("lon", ramp['lon'])
+            Parking.set ("heading", ramp['heading'])
+            #dummy below
+            Parking.set ("type", "gate")
+            Parking.set ("number", "")
+            Parking.set ("radius", "40")
+            Parking.set ("airlineCodes", "")
+        
+        TaxiNodes = ET.SubElement (groundnet, "TaxiNodes")
+        for point in TaxiRouteNode:
+            node = ET.SubElement(TaxiNodes, "node")
+            node.set ("index", point['id'])
+            node.set ("lat", point['lat'])
+            node.set ("lon", point['lon'])
+            node.set ("isOnRunway", "0")
+            node.set ("holdPointType", "none")
+        
+        TaxiWaySegments = ET.SubElement (groundnet, "TaxiWaySegments")
+        for route in TaxiRoute:
             arc = ET.SubElement (TaxiWaySegments, "arc")
-            arc.set ("begin", route['end'])
-            arc.set ("end", route['begin'])
+            arc.set ("begin", route['begin'])
+            arc.set ("end", route['end'])
             arc.set ("isPushBackRoute", "0")
             arc.set ("name", route['name'])
-    
-    #Save
-    basedir = os.getcwd()
-    file_groundnet = open (os.path.join (basedir,  "newGroundNet.xml"), "w")
-    ps = parseString (ET.tostring (groundnet))
-    file_groundnet.writelines (ps.toprettyxml(indent=" "))
-    file_groundnet.close()
+        
+            #reverse arc
+            if route['oneway'] <> "1":
+                arc = ET.SubElement (TaxiWaySegments, "arc")
+                arc.set ("begin", route['end'])
+                arc.set ("end", route['begin'])
+                arc.set ("isPushBackRoute", "0")
+                arc.set ("name", route['name'])
+        
+        #Save
+        basedir = os.getcwd()
+        file_groundnet = open (os.path.join (basedir,  airport_icao + ".groundnet.xml"), "w")
+        ps = parseString (ET.tostring (groundnet))
+        file_groundnet.writelines (ps.toprettyxml(indent="  "))
+        file_groundnet.flush()
+        file_groundnet.close()
     
     return 0
 
